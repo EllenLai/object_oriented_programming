2eb4429fb72b4706857b6a3ce61141e910e94f79
EllenLai <miss.ellenlai@gmail.com>
2014-01-16 14:11:15 -0500

    creating parent Person class

--- people.rb
+++ people.rb
@@ -1,12 +1,21 @@
+class Person
+	def initialize(name)
+		@name = name
+	end
+end
+
 class Student
 	def learn
-		puts "I get it!"
+	puts "I get it!"
 	end
 end
 
 
 class Instructor
 	def teach
-		puts "Everything in Ruby is an Object"
+	puts "Everything in Ruby is an Object"
 	end
 end
+
+ellen = Person.new("Ellen")
+
