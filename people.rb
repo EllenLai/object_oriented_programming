fa93df84e5e94fb3b7eb1d3a74e32064a9dc9d1a
EllenLai <miss.ellenlai@gmail.com>
2014-01-16 14:40:14 -0500

    created an instance for Chris

--- people.rb
+++ people.rb
@@ -6,23 +6,23 @@ class Person
 	end
 
 	def greeting
-		puts "Hi my name is #{name}"
+		puts "Hi my name is #{@name}"
 	end
 end
 
 
-class Student
+class Student < Person
 	def learn
 		puts "I get it!"
 	end
 end
 
 
-class Instructor
+class Instructor < Person
 	def teach
 		puts "Everything in Ruby is an Object"
 	end
 end
 
-ellen = Person.new("Ellen")
-
+chris = Instructor.new("Chris")
+chris.greeting
