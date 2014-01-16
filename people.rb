9174f4a93e08b6e1e7ecb759928c8fe8bab8336a
EllenLai <miss.ellenlai@gmail.com>
2014-01-16 14:33:00 -0500

    adding greeting

--- people.rb
+++ people.rb
@@ -1,19 +1,26 @@
+
+
 class Person
 	def initialize(name)
 		@name = name
 	end
+
+	def greeting
+		puts "Hi my name is #{name}"
+	end
 end
+
 
 class Student
 	def learn
-	puts "I get it!"
+		puts "I get it!"
 	end
 end
 
 
 class Instructor
 	def teach
-	puts "Everything in Ruby is an Object"
+		puts "Everything in Ruby is an Object"
 	end
 end
 
